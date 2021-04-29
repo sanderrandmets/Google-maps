cf083963c64e81ca4a40fd9827e6d2d96b7464bf
Sander <sander91112@gmail.com>
2021-04-29 12:03:50 +0300

    Make database and maps

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google maps</title>
+</head>
+<body>
+    <form action="insert.php" method="POST">
+        <input type="text" name="name" placeholder="name">
+        <input type="text" name="latitude" placeholder="latitude">
+        <input type="text" name="longitude" placeholder="longitude">
+        <input type="text" name="description" placeholder="description">
+        <button type="submit">Lisa</button>
+    </form>
+</body>
+</html>
